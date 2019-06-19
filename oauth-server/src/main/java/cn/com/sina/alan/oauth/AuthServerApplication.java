50085f9df7072376d2b3d2202d5e62053e35fdea
oujiaming2018 <438661727@qq.com>
2019-06-19 17:22:28 +0800

    first commit

---
+++ oauth-server/src/main/java/cn/com/sina/alan/oauth/AuthServerApplication.java
@@ -0,0 +1,15 @@
+package cn.com.sina.alan.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by on 2017.03.08.
+ */
+@SpringBootApplication
+public class AuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApplication.class, args);
+    }
+}
